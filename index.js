57d9e87c5e82beb2253ad1883e1fb250f683a3c1
Joaquin Godoy <godoy.joaquin1997@gmail.com>
2025-08-19 05:00:39 -0300

    finished project 5 out of 5. It was only about documentation last challenge

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const { originalname, mimetype, size } = req.file;
+  // console.log('Filename:', originalname);
+  // console.log('MIME type:', mimetype);
+  // console.log('Size (bytes):', size);
+  res.json({name: originalname, type: mimetype, size: size})
+})
 
 
 
